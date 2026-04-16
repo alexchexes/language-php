562fab2a0a492dda7c99df4019698084b0330372
Alex C <alexchexes@gmail.com>
2026-04-16 20:57:34 +0100

    test: add more cases to interpolation tmgrammar test

--- spec/tmgrammar/php-regexp/interpolation.php
+++ spec/tmgrammar/php-regexp/interpolation.php
@@ -45,6 +45,7 @@
 #     ^ variable.other.php punctuation.definition.variable.php
 
  "/a([a-{$b(1-2, '[a-b]')}])/";
+#   ^                      ^ punctuation.definition.group.regexp.php
 #    ^                    ^ meta.embedded.character-class.regexp.php punctuation.definition.character-class.regexp.php
 #     ^^^^^^^^^^^^^^^^^^^^ string.regexp.character-class.php constant.other.character-class.set.regexp.php
 #      ^ constant.other.character-class.set.regexp.php keyword.operator.range.regexp.php
@@ -54,27 +55,99 @@
 #           ^ ^ meta.function-call.invoke.php constant.numeric.decimal.php
 #            ^ meta.function-call.invoke.php keyword.operator.arithmetic.php
 #                 ^^^^^ meta.function-call.invoke.php string.quoted.single.php
-
-// actual example with whole interpolation expression replaced with an "atom" regex token:
- "/a([a-\x{42}])/";
-#    ^^^^^^^^^^ source.php meta.embedded.regexp.php string.regexp.double-quoted.php meta.embedded.group.regexp.php string.regexp.double-quoted.php meta.embedded.character-class.regexp.php
-#    ^        ^ meta.embedded.character-class.regexp.php punctuation.definition.character-class.regexp.php
-#     ^^^^^^^^ string.regexp.character-class.php constant.other.character-class.set.regexp.php
-#      ^ constant.other.character-class.set.regexp.php keyword.operator.range.regexp.php
-#       ^^^^^^ constant.other.character-class.set.regexp.php constant.character.numeric.regexp.php
+#                       ^ - punctuation.definition.group.regexp.php
 
 <<<REGEX
-
 # Extracted from - should tokenize interpolation inside REGEXP heredoc groups
 
    a*$b-c*$\n
+#  ^^^^^^^^^^ source.php meta.embedded.regexp.php string.regexp.heredoc.php
+#   ^    ^ keyword.operator.quantifier.regexp.php
+#    ^^ string.regexp.heredoc.php variable.other.php
+#    ^ variable.other.php punctuation.definition.variable.php
 #      ^^^^^^ - variable.other.php
+#         ^ keyword.control.anchor.regexp.php
+
+   a*{$b}-c*$\n}
+#  ^^^^^^^^^^^^^ source.php meta.embedded.regexp.php string.regexp.heredoc.php
+#   ^      ^ string.regexp.heredoc.php keyword.operator.quantifier.regexp.php
+#    ^  ^ string.regexp.heredoc.php punctuation.definition.variable.php
+#     ^^ string.regexp.heredoc.php variable.other.php
+#     ^ variable.other.php punctuation.definition.variable.php
+#        ^^^^^^ - variable.other.php
+#           ^ keyword.control.anchor.regexp.php
+
+   a*{1}({$b('a*{1}')}c*$\n})
+#   ^^^^               ^ keyword.operator.quantifier.regexp.php
+#       ^                   ^ meta.embedded.group.regexp.php punctuation.definition.group.regexp.php
+#        ^^^^^^^^^^^^^^^^^^^ meta.embedded.group.regexp.php string.regexp.heredoc.php
+#        ^^          ^ string.regexp.heredoc.php punctuation.definition.variable.php
+#          ^ variable.other.php
+#            ^^^^^^^ meta.function-call.invoke.php string.quoted.single.php
+#              ^^^^ - keyword.operator.quantifier.regexp.php punctuation.definition.variable.php
+#                   ^ - punctuation.definition.group.regexp.php
+#                     ^^^^^^^^ - variable.other.php punctuation.definition.variable.php meta.embedded.interpolation.php
+#                       ^ keyword.control.anchor.regexp.php
+# TODO Uncomment this when KapitanOczywisty language-php pull 43 is merged
+# #        ^^^^^^^^^^^^^ meta.embedded.interpolation.php
+
+# Range endpoints should hand off cleanly into interpolation.
+
+   [a-$b]
+#  ^^^^^^ source.php meta.embedded.regexp.php string.regexp.heredoc.php meta.embedded.character-class.regexp.php
+#  ^    ^ meta.embedded.character-class.regexp.php punctuation.definition.character-class.regexp.php
+#   ^^^^ string.regexp.character-class.php constant.other.character-class.set.regexp.php
+#    ^ constant.other.character-class.set.regexp.php keyword.operator.range.regexp.php
+#     ^^ constant.other.character-class.set.regexp.php variable.other.php
+#     ^ variable.other.php punctuation.definition.variable.php
+
+   a([a-{$b(1-2, '[a-b]')}])
+#   ^                      ^ punctuation.definition.group.regexp.php
+#    ^                    ^ meta.embedded.character-class.regexp.php punctuation.definition.character-class.regexp.php
+#     ^^^^^^^^^^^^^^^^^^^^ string.regexp.character-class.php constant.other.character-class.set.regexp.php
+#      ^ constant.other.character-class.set.regexp.php keyword.operator.range.regexp.php
+#       ^                ^ constant.other.character-class.set.regexp.php punctuation.definition.variable.php
+#        ^^ constant.other.character-class.set.regexp.php meta.function-call.invoke.php variable.other.php
+#        ^ variable.other.php punctuation.definition.variable.php
+#           ^ ^ meta.function-call.invoke.php constant.numeric.decimal.php
+#            ^ meta.function-call.invoke.php keyword.operator.arithmetic.php
+#                 ^^^^^ meta.function-call.invoke.php string.quoted.single.php
+#                       ^ - punctuation.definition.group.regexp.php
 REGEX;
 
  '/a*$b-c*$\n/';
 #  ^^^^^^^^^^ - variable.other.php
 
+ '/a*$b-c*$\n/';
+#  ^^^^^^^^^^ - variable.other.php
+
+ '/a*{$b}-c*$\n}/';
+#  ^^^^^^^^^^^^^ - variable.other.php
+
+ '/a*{1}({$b("a*{1}")}c*$\n})/';
+#  ^^^^^^^^^^^^^^^^^^^^^^^^^^ - variable.other.php
+
+ '/[a-$b]/';
+#  ^^^^^^ - variable.other.php
+
+ '/a([a-{$b(1-2, "[a-b]")}])/';
+#  ^^^^^^^^^^^^^^^^^^^^^^^^^ - variable.other.php
+
 <<<'REGEX'
+
    a*$b-c*$\n
 #  ^^^^^^^^^^ - variable.other.php
+
+   a*{$b}-c*$\n}
+#  ^^^^^^^^^^^^^ - variable.other.php
+
+   a*{1}({$b('a*{1}')}c*$\n})
+#  ^^^^^^^^^^^^^^^^^^^^^^^^^^ - variable.other.php
+
+   [a-$b]
+#  ^^^^^^ - variable.other.php
+
+   a([a-{$b(1-2, '[a-b]')}])
+#  ^^^^^^^^^^^^^^^^^^^^^^^^^ - variable.other.php
+
 REGEX;
